aff5926bce30bd2f947b811dfaa8124a44145ec3
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-01-09 11:55:25 +0000

    bob

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const palindrome = s => s == s.split('').reverse().join('')
 
 const vowelCount = s => s.toLowerCase().replace(/[^aeiou]/gm, '').length
 
+const upperCount = s => s.replace(/[^A-Z]/gm, '').length
 
-const compute = (left, operator, right) => {	"+": sum, "-": sub, "*": mul, "/": div}[operator](left, right)
-	
+const numCount = s => s.replace(/[^0-9]/gm, '').length
+
+const ops = {"+":sum, "-": sub, "*": mul, "/": div}
+const compute = (left, operator, right) => ops[operator](left, right)
+
 
 
